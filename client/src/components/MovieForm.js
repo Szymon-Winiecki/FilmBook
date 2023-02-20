e8222d252ba51348ec01d971abedfc8da8eb86f4
Szymon-Winiecki <Szymon.win4@gmail.com>
2023-02-20 17:19:41 +0100

    fixes in MovieForm

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -40,6 +40,10 @@ class MovieForm extends React.Component {
     }
 
     submit() {
+
+        const user = this.context;
+        if(!user || !user.accessToken) return;
+
         let movieData = {
             'tytul_orginalny': document.querySelector('#movie-title-original').value,
             'tytul_polski': document.querySelector('#movie-title-local').value,
@@ -47,12 +51,15 @@ class MovieForm extends React.Component {
             'data_polskiej_premiery': document.querySelector('#movie-release-year-local').value,
             'czas_trwania': parseInt(document.querySelector('#movie-duration-h').value) * 60 + parseInt(document.querySelector('#movie-duration-m').value),
             'opis': document.querySelector('#movie-description').value,
-            'czlowiek_kina_id': this.state.movieDirector?.id
+            'czlowiek_kina_id': this.state.movieDirector?.id ? this.state.movieDirector.id : null
         };
-        let url = `http://${const_props.API_ADDR}:${const_props.API_PORT}/api/movie/${this.props.movie ? 'update/' + this.props.movie.id : ''}`;
+        let url = `http://${const_props.API_ADDR}:${const_props.API_PORT}/api/movie/${this.props.movie ? '/' + this.props.movie.id : ''}`;
         fetch(url, {
                 method: this.props.movie ? 'PUT' : 'POST',
-                headers: { 'Content-Type': 'application/json' },
+                headers: { 
+                    'Content-Type': 'application/json',
+                    Authentication: `Bearer ${user.accessToken}`
+                },
                 credentials: 'include',
                 body: JSON.stringify(movieData)
             });
@@ -92,41 +99,41 @@ class MovieForm extends React.Component {
                 <div>
                     <label htmlFor="movie-title-original">Tytuł oryginalny:</label>
                     <input className='s-input' type="text" id="movie-title-original" key={this.props.movie ? this.props.movie.id : ""}
-                        defaultValue={this.props.movie ? this.props.movie.tytul_orginalny : ""} />
+                        defaultValue={this.props.movie?.tytul_orginalny ? this.props.movie.tytul_orginalny : ""} />
                 </div>
 
                 <div>
                     <label htmlFor="movie-title-local">Tytuł polski:</label>
                     <input className='s-input' type="text" id="movie-title-local" key={this.props.movie ? this.props.movie.id : ""}
-                        defaultValue={this.props.movie ? this.props.movie.tytul_polski : ""} />
+                        defaultValue={this.props.movie?.tytul_polski ? this.props.movie.tytul_polski : ""} />
                 </div>
 
                 <div>
                     <label htmlFor="movie-release-year">Premiera światowa:</label>
                     <input className='s-input' type="date" id="movie-release-year" key={this.props.movie ? this.props.movie.id : ""}
-                        defaultValue={this.props.movie ? this.props.movie.data_swiatowej_premiery.substring(0, 10) : ""} />
+                        defaultValue={this.props.movie?.data_swiatowej_premiery ? this.props.movie.data_swiatowej_premiery.substring(0, 10) : ""} />
                 </div>
 
                 <div>
                     <label htmlFor="movie-release-year-local">Premiera polska:</label>
                     <input className='s-input' type="date" id="movie-release-year-local" key={this.props.movie ? this.props.movie.id : ""}
-                        defaultValue={this.props.movie ? this.props.movie.data_polskiej_premiery.substring(0, 10) : ""} />
+                        defaultValue={this.props.movie?.data_polskiej_premiery ? this.props.movie.data_polskiej_premiery.substring(0, 10) : ""} />
                 </div>
 
                 <div>
                     <label htmlFor="movie-duration-h">Czas trwania:</label>
                     <div className='movie-duration-inputs-section'>
                         <input className='s-input movie-duration' type="number" min='0' id="movie-duration-h" key={this.props.movie ? this.props.movie.id : "1"}
-                            defaultValue={this.props.movie ? Math.floor(this.props.movie.czas_trwania / 60) : 0} /><span> godz. </span>
+                            defaultValue={this.props.movie?.czas_trwania ? Math.floor(this.props.movie.czas_trwania / 60) : 0} /><span> godz. </span>
                         <input className='s-input movie-duration' type="number" min='0' max='59' id="movie-duration-m" key={this.props.movie ? -this.props.movie.id : "2"}
-                            defaultValue={this.props.movie ? this.props.movie.czas_trwania - (60 * Math.floor(this.props.movie.czas_trwania / 60)) : 0}/><span> min. </span>
+                            defaultValue={this.props.movie?.czas_trwania ? this.props.movie.czas_trwania - (60 * Math.floor(this.props.movie.czas_trwania / 60)) : 0}/><span> min. </span>
                     </div>
                 </div>
 
                 <div>
                     <label htmlFor="movie-description">Opis:</label>
                     <textarea className='s-input' type="time" id="movie-description" key={this.props.movie ? this.props.movie.id : ""}
-                        defaultValue={this.props.movie ? this.props.movie.opis : null}></textarea>
+                        defaultValue={this.props.movie?.opis ? this.props.movie.opis : null}></textarea>
                 </div>
 
                 <div>
